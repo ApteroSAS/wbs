97ca2c6e268e76b1e6fba2ab37ab376af75885c9
Pierre-Henri AORUS <pierre.henri@aptero.co>
2021-06-04 10:27:05 +0200

    new version of wbs-light #elya

---
+++ public/wbs-light/src/index.js
@@ -0,0 +1,58 @@
+var EventEmitter = require('events').EventEmitter
+var inherits = require('inherits')
+
+var Display = require('./display/display')
+var mixer = require('./lib/mixer')
+var InputManager = require('./lib/inputmanager')
+var rtmp = require('./lib/rtmp')
+
+inherits(WBS, EventEmitter)
+
+function WBS (element, opts) {
+  var self = this
+  if (!(self instanceof WBS)) return new WBS(element, opts)
+
+  if (typeof element === 'string') {
+    element = document.querySelector(element)
+  }
+
+  opts = opts || {}
+  self.opts = opts;
+  
+  var audioContext = new AudioContext()
+
+  mixer.setAudioContext(audioContext)
+  opts.output = opts.output || {
+    width: 1280,
+    height: 720,
+    fps: 24,
+    audioContext: audioContext
+  }
+  opts.inputs = opts.inputs || []
+  opts.injectStyles = opts.injectStyles || true
+
+  if (opts.injectStyles) require('./../less/wbs.css')
+  self.inputManager = new InputManager(opts)
+ 
+  //self._display = new Display(element, opts)
+
+  /*rtmp.onStop = ()=>{
+    console.log("Stopped From Server");
+    self._display.controls.stopStream();
+  };
+  rtmp.onAlreadyInUse = ()=>{
+    console.log("Already in use");
+    alert("RTMP flux is already in use");
+  };
+  self._display.on('stream', function (stream) {
+    self.emit('stream', stream)
+    rtmp.connectServer();
+    rtmp.startStreaming(stream);
+  })
+  self._display.on('stopstream', function () {
+    self.emit('stopstream')
+    rtmp.stopStreaming();
+  })*/
+}
+
+module.exports = WBS
